4626b97a95ad48cf0407674d771dff06b6986a37
Andy <andy_lai314@hotmail.com>
2025-04-02 02:06:15 -0700

    Added if __name__ == "__main__":

--- eurooptprice.py
+++ eurooptprice.py
@@ -1,25 +1,33 @@
 import argparse
 from decimal import Decimal
 
-parser = argparse.ArgumentParser()
-parser.add_argument("spot", help="spot price of the underlying asset", type=float)
-parser.add_argument("strike", help="strike price of the option", type=float)
-parser.add_argument("option", choices=["call", "put"], help="choose call or put option")
-args = parser.parse_args()
-
-spot = args.spot
-strike = args.strike
-        
-if args.option == "call":
-    if args.spot <= args.strike:
+def call(spot, strike):
+    if spot <= strike:
         print("The call option is out of the money, and it is worth $0.")
     else:
         price = Decimal(str(spot)) - Decimal(str(strike))
         print(f"The price of the call option should be $ {price}.")
 
-elif args.option == "put":
-    if args.spot >= args.strike:
+def put(spot, strike):
+    if spot >= strike:
         print("The put option is out of the money, and it is worth $0.")
     else:
         price = Decimal(str(strike)) - Decimal(str(spot))
         print(f"The price of the put option should be $ {price}.")
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("spot", help="spot price of the underlying asset", type=float)
+    parser.add_argument("strike", help="strike price of the option", type=float)
+    parser.add_argument("option", choices=["call", "put"], help="choose call or put option")
+    args = parser.parse_args()
+
+    spot = args.spot
+    strike = args.strike
+            
+    if args.option == "call":
+        call(spot, strike)
+
+    elif args.option == "put":
+        put(spot, strike)
