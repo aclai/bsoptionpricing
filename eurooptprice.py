2633841c54abb209d976c8dc00146cd12176a4a0
Andy <andy_lai314@hotmail.com>
2025-04-21 01:29:39 -0700

    Added long/short & made other misc adjustments

--- eurooptprice.py
+++ eurooptprice.py
@@ -1,33 +1,46 @@
+"""
+This will calculate the value of a position on an European option on the expiration day.
+
+Usage:
+python eurooptprice.py [-h] -sp SPOT -st STRIKE -o {call,put} -p {long,short} 
+
+Example 1:
+If one holds a long position on a call option with a strike price of $10 and spot price of $20 - python eurooptprice.py -sp 20 -st 10 -o call -p long
+
+Example 2:
+If one holds a short position on a put option with a strike price of $30 and spot price of $25 - python eurooptprice.py -sp 25 -st 30 -o put -p short
+"""
 import argparse
 from decimal import Decimal
-
-def call(spot, strike):
+def call(spot, strike, position):
     if spot <= strike:
-        print("The call option is out of the money, and it is worth $0.")
+        print("The call option is out of the money, and your position is $0.")
     else:
         price = Decimal(str(spot)) - Decimal(str(strike))
-        print(f"The price of the call option should be $ {price}.")
-
-def put(spot, strike):
+        if position == "long":
+            print(f"A long position of the call option should be $ {price}.")
+        else:
+            print(f"A short position of the call option should be $ {-price}.")
+def put(spot, strike, position):
     if spot >= strike:
-        print("The put option is out of the money, and it is worth $0.")
+        print("The put option is out of the money, and your position is $0.")
     else:
         price = Decimal(str(strike)) - Decimal(str(spot))
-        print(f"The price of the put option should be $ {price}.")
-
+        if position == "long":
+            print(f"A long position of the put option should be $ {price}.")
+        else:
+            print(f"A short position of the put option should be $ {-price}.")
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument("spot", help="spot price of the underlying asset", type=float)
-    parser.add_argument("strike", help="strike price of the option", type=float)
-    parser.add_argument("option", choices=["call", "put"], help="choose call or put option")
+    parser.add_argument("-sp", "--spot", help="spot price of the underlying asset", type=float, required=True)
+    parser.add_argument("-st", "--strike", help="strike price of the option", type=float, required=True)
+    parser.add_argument("-o", "--option", choices=["call", "put"], help="choose call or put option", required=True)
+    parser.add_argument("-p", "--position", choices=["long", "short"], help="choose long or short position", required=True)
     args = parser.parse_args()
-
     spot = args.spot
     strike = args.strike
-            
+    position = args.position
     if args.option == "call":
-        call(spot, strike)
-
+        call(spot, strike, position)
     elif args.option == "put":
-        put(spot, strike)
+        put(spot, strike, position)
